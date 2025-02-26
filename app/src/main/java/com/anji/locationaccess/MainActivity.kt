dd9a2a24ed38a50ac78c43d4080bd6d09e38e559
anjikumarg <gollapalli.anjikumar@gmail.com>
2025-02-26 20:32:46 +0530

    project setup

---
+++ app/src/main/java/com/anji/locationaccess/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.anji.locationaccess
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.anji.locationaccess.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.root)
+    }
+}
